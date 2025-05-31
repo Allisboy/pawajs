c3829bd180f2f34cc202ad29adc297cdd91e0beb
Allisboy <136527037+Allisboy@users.noreply.github.com>
2025-05-31 19:25:45 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,150 @@
-import { ComponentProps } from "./classes.js";
+export const splitAndAdd=(string) => {
+   const strings=string.split('-')
+  const join=strings[0]+strings[1]
+  return join.toUpperCase()
+}
 
+export const processNode = (node, itemContext) => {
+  
+  if (typeof itemContext === 'number') {
+    return
+  }
+  
+  if (node.nodeType === 3) { // Text node
+    const text = node.textContent
+    const newText = text.replace(/{{(.+?)}}/g, (match, exp) => {
+      try {
+        const keys = Object.keys(itemContext)
+        
+        const values = keys.map(key => itemContext[key])
+        
+        return new Function(...keys, `return ${exp}`)(...values)
+      } catch {
+        return match
+      }
+    })
+    //console.log(newText)
+    node.textContent = newText
+  } else if (node.attributes) {
+    Array.from(node.attributes).forEach(attr => {
+      const newValue = attr.value.replace(/{{(.+?)}}/g, (match, exp) => {
+        try {
+          const keys = Object.keys(itemContext)
+          const values = keys.map(key => itemContext[key])
+          return new Function(...keys, `return ${exp}`)(...values)
+        } catch {
+          return match
+        }
+      })
+      attr.value = newValue
+    })
+  }
+  Array.from(node.childNodes).forEach((n) => {
+    processNode(n, itemContext)
+  })
+}
+
+
+export const evaluation=(context,exp) => {
+    try {
+      const keys = Object.keys(context);
+const resolvePath = (path, obj) => {
+    return path.split('.').reduce((acc, key) => acc?.[key], obj);
+};
+const values = keys.map((key) => resolvePath(key, context));
+return new Function(...keys,`return ${exp}`)(...values)
+    } catch (e) {
+      throw e
+    }
+}
+
+
+export const propsValidator=(obj={},propsAttri,name)=>{
+  let newObj={}
+  
+  const jsTypes=['Array','String','Number']
+  for (const[key,value] of Object.entries(obj)) {
+    const propsValue=propsAttri[key]
+    if(typeof value === 'object'){
+      if(propsAttri[key] || propsAttri[key] === 0){
+        const checker=ComponentProps(propsAttri[key],value?.err,name)
+        if (value.type) {
+        checker[value.type.name]()
+        }
+      }else{
+        if (value.strict) {
+          console.warn(`undefined props ${key} at ${name} component props is needed`)
+          throw new Error(value.err+ ' the props is needed' || `props undefined ${name}`);
+        }else{
+          if (value.default || value.default === 0) {
+            propsAttri[key]=value.default
+          }
+        }
+
+      }
+    }
+  }
+  return {...propsAttri}
+}
+export const sanitizeTemplate = (temp) => {
+  if (typeof temp !== 'string') return '';
+  return temp.replace(/<script\b[^>]*>([\s\S]*?)<\/script>/gi, '');
+};
+
+export const ComponentProps=(some,message,name)=>{
+
+    return({
+    Array:()=>{
+
+        if (Array.isArray(some)) {
+            return true
+        }else{
+            throw new Error(message ?message + ' / Not type of an Array ': `${some} must be an array at ${name} component`);
+
+        }
+    },
+    String:()=>{
+        if (typeof some === 'string') {
+            return true
+        }else{
+            throw new Error(message? message + ' / Not type of a String' :`${some} must be a string at ${name} component`);
+
+        }
+    },
+    Number:()=>{
+        if (typeof some === 'number') {
+            return true
+        }else{
+            throw new Error(message? message+' / Not type of a Number ': `${some} must be a number at ${name} component`);
+
+        }
+    },
+    Object:()=>{
+        if (typeof some === 'object') {
+            return true
+        }else{
+            throw new Error(message? message+' / Not type of an Object ' :`${some} must be an object at ${name} component`);
+
+        }
+    },
+    Function:()=>{
+        if (typeof some === 'function') {
+            return true
+        }else{
+            throw new Error(message? message+' / Not type of a Function ': `${some} must be a function at ${name} component`);
+        }
+    },
+    Boolean:()=>{
+        if (typeof some === 'boolean') {
+            return true
+        }else{
+            throw new Error(message? message+' / Not type of a Boolean ' :`${some} must be a Boolean at ${name} component`);
+
+        }
+    },
+})
+
+}
 export const replaceOperators = (expression) => {
   return expression
     .replace(/#/g, '=')
@@ -9,166 +154,3 @@ export const replaceOperators = (expression) => {
     .replace(/::(.)(?=.)/g, (_, char) => char.toUpperCase())
     .replace(/%/g, '"');
 };
-
-export function unwrap(element) {
-  // Create a document fragment to hold the children
-  const fragment = document.createDocumentFragment();
-  
-  // Move all children to the fragment
-  while (element.firstChild) {
-    fragment.appendChild(element.firstChild);
-  }
-  
-  if(element.parentNode === null) return
-  // Replace the element with all its children at once
-  element.parentNode.replaceChild(fragment, element);
-}
-
-const templateEngine = (template, propsAttri) => {
-  const regex = /#{(.*?)}/g;
-  const resolvePath = (path, obj) => {
-    return path.split('.').reduce((acc, key) => acc?.[key], obj);
-  };
-  const keys = Object.keys(propsAttri);
-  return template.replace(regex, (match, key) => {
-    const values = keys.map((key) => resolvePath(key, propsAttri));
-    const condition= new Function(...keys, `return ${key}`)(...values);
-    // const value = key.trim().split('.').reduce((obj, path) => obj?.[path], propsAttri);
-    return condition ?? '';
-  });
-}
-
-
-const createTemplate = (html, props) => {
-  if (templates.has(html)) {
-    return templateEngine(templates.get(html), props);
-  }
-  
-  templates.set(html, html);
-  return templateEngine(html, props);
-}
-
-
-export const elementTrack=(el)=>{
-  const mounted=el.isConnected
-   const track={
-    el:el,
-    mounted:mounted,
-    transition:el.getAttribute('enter') ? true :false,
-    duration:el.getAttribute('duration') || 300,
-    beforeEnter: (callback) => {
-      if(track.transition){
-        const enter=el.getAttribute('enter')
-        enter?.split(' ').forEach(cl => {
-          if(cl === '')return
-          el.classList.add(cl)
-        })
-      }
-      if(callback) callback(el)
-        return track
-    },
-    enter:(callback)=>{
-      if(track.transition){
-        const enter=el.getAttribute('enter')
-        const enterTo=el.getAttribute('enter-to')
-        setTimeout(() => {
-          requestAnimationFrame(() => {
-            enter?.split(' ').forEach(cl => {
-              if(cl === '')return
-              el.classList.remove(cl)
-            })
-            
-            enterTo?.split(' ').forEach(cl => {
-              if(cl === '')return
-              el.classList.add(cl) 
-            })
-          })
-          // console.log(children);
-        }, track.duration);
-      }
-      
-        return track
-    },
-    beforeLeave:(callback)=>{
-      if (track.transition) {
-        const enterTo=el.getAttribute('enter-to')
-        const leaveTo=el.getAttribute('leave-to')
-        enterTo?.split(' ').forEach(cl => {
-          if(cl === '')return
-            el.classList.remove(cl) 
-          })
-          requestAnimationFrame(() => {
-            leaveTo?.split(' ').forEach(cl => {
-              if(cl === '')return
-              el.classList.add(cl)
-            })
-          })
-      }
-     if(callback) callback(el)
-      return track
-    },
-    leave:(callback)=>{
-      if(track.transition){
-        const leaveTo=el.getAttribute('leave-to')
-        const leave=el.getAttribute('leave')
-        const enter=el.getAttribute('enter')
-        leaveTo?.split(' ').forEach(cl => {
-          if(cl === '')return
-          el.classList.add(cl)
-        })
-        requestAnimationFrame(() => {
-          leave?.split(' ').forEach(cl => {
-            if(cl === '')return
-            el.classList.add(cl)
-          })
-        })
-        setTimeout(() => {
-          leave?.split(' ').forEach(cl => {
-            if(cl === '')return
-            el.classList.remove(cl)
-          })
-        }, track.duration);
-      }
-      if(callback && track.transition) {
-        setTimeout(() => {
-          callback(el)
-        }, track.duration);
-      }else if(callback && track.transition === false){
-        callback(el)
-      }
-        return track
-    },
-  }
-  return track
- }
-
- const propsValidator=(obj={},propsAttri)=>{
-  let newObj={}
-  const jsTypes=['Array','String','Number']
-  for (const[key,value] of Object.entries(obj)) {
-    const propsValue=propsAttri[key]
-    if(typeof value === 'object'){
-      if(propsAttri[key] || propsAttri[key] === 0){
-        const checker=ComponentProps(propsAttri[key],value?.err)
-        if (value.type) {
-        checker[value.type.name]()
-        }
-      }else{
-        if (value.strict) {
-          console.log('entered here',value)
-          throw new Error(value.err+ ' the props is needed' || 'props undefined');
-        }else{
-          if (value.default || value.default === 0) {
-            propsAttri[key]=value.default
-          }
-        }
-        
-      }
-    }
-  }
-  return propsAttri
-}
-
-
-
-export { createTemplate, templateEngine,propsValidator };
